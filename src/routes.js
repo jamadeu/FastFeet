b87692178a9b300a142bf4f24aca2399a9bc869a
jamadeu <jean.amadeu2@gmail.com>
2020-02-25 07:45:55 -0300

    Authmiddleware

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,13 @@ import { Router } from 'express';
 
 import SessionController from './app/controllers/SessionController';
 import UserController from './app/controllers/UserController';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
 
 export default routes;
