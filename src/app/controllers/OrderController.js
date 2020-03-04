ed272d95107ec54a1c035f99bfe0d9e9b9d36f38
jamadeu <jean.amadeu2@gmail.com>
2020-03-04 05:49:38 -0300

    OrderController store

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -19,16 +19,35 @@ class OrderController {
       return res.status(400).json({ error: 'Validation fail' });
     }
 
-    // validar se recipient e deliveryman existem
+    const recipientExists = await Recipient.findByPk(req.body.recipient_id);
+
+    if (!recipientExists) {
+      return res.status(400).json({ error: 'Recipient not found.' });
+    }
+
+    const deliveryManExists = await Recipient.findByPk(req.body.deliveryman_id);
+
+    if (!deliveryManExists) {
+      return res.status(400).json({ error: 'Deliveryman not found.' });
+    }
 
     const { id } = await Order.create(req.body);
 
     const order = await Order.findByPk(id, {
+      attributes: [
+        'id',
+        'product',
+        'canceled_at',
+        'start_date',
+        'end_date',
+        'signature_id',
+      ],
       include: [
         {
           model: Recipient,
           as: 'recipient',
           attributes: [
+            'id',
             'name',
             'street',
             'number',
@@ -41,14 +60,15 @@ class OrderController {
         {
           model: DeliveryMan,
           as: 'deliveryman',
-          attributes: ['name', 'email'],
+          attributes: ['id', 'name', 'email'],
         },
       ],
     });
-    // await Notification.create({
-    //   content: `Encomenda de ${name} disponível para coleta`,
-    //   user: req.body.deliveryman_id,
-    // });
+
+    await Notification.create({
+      content: `Nova encomenda disponível para coleta`,
+      deliveryman_id: req.body.deliveryman_id,
+    });
 
     await Queue.add(OrderingMail.key, {
       order,
